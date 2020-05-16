b67b152186951e2114c9efa0fabbd64871a78d4e
Diaa Eldeen Esmail Ahmed <diaaeldean56@gmail.com>
2020-05-16 17:48:09 +0200

    Fixed the CMakeLists bug

--- Toolchain/cmake_arm_none_eabi.cmake
+++ Toolchain/cmake_arm_none_eabi.cmake
@@ -1,4 +1,4 @@
-message(STATUS ("Processing the arm-none-eabi tool chain file")
+message(STATUS "Processing the arm-none-eabi tool chain file")
 
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
@@ -14,17 +14,7 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 
-# ---------------------------- Error -----------------------------------------
-# nosys.specs: attempt to rename spec 'link_gcc_c_sequence' to already
-# defined spec 'nosys_link_gcc_c_sequence'
-# set(CMAKE_C_FLAGS "-specs=nosys.specs"   CACHE INTERNAL "")
-# Caused by adding it twice in the linker flags or adding it to the compile flags
-# ----------------------------------------------------------------------------
-
 set(CMAKE_EXE_LINKER_FLAGS "-specs=nosys.specs"   CACHE INTERNAL "")
-
- unset(CMAKE_EXECUTABLE_SUFFIX)
-set(CMAKE_EXECUTABLE_SUFFIX ".out" CACHE INTERNAL FORCE "")
 
 set(CMAKE_ASM_COMPILER_ID GNU)
 set(CMAKE_C_COMPILER_ID GNU)
