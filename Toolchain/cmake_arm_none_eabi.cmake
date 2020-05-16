a34417d5be61e1002ebc9bf33e7d8b8e4ba06b13
Diaa Eldeen Esmail Ahmed <diaaeldean56@gmail.com>
2020-05-16 14:51:13 +0200

    Added CMake lists files for building the project

---
+++ Toolchain/cmake_arm_none_eabi.cmake
@@ -0,0 +1,33 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+
+# ---------------------------- Error -----------------------------------------
+# nosys.specs: attempt to rename spec 'link_gcc_c_sequence' to already
+# defined spec 'nosys_link_gcc_c_sequence'
+# set(CMAKE_C_FLAGS "-specs=nosys.specs"   CACHE INTERNAL "")
+# Caused by adding it twice in the linker flags or adding it to the compile flags
+# ----------------------------------------------------------------------------
+
+set(CMAKE_EXE_LINKER_FLAGS "-specs=nosys.specs"   CACHE INTERNAL "")
+
+ unset(CMAKE_EXECUTABLE_SUFFIX)
+set(CMAKE_EXECUTABLE_SUFFIX ".out" CACHE INTERNAL FORCE "")
+
+set(CMAKE_ASM_COMPILER_ID GNU)
+set(CMAKE_C_COMPILER_ID GNU)
+set(CMAKE_CXX_COMPILER_ID GNU)
+
+set(CMAKE_ASM_COMPILER_FORCED TRUE)
+set(CMAKE_C_COMPILER_FORCED TRUE)
+set(CMAKE_CXX_COMPILER_FORCED TRUE)
