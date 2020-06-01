5a7894ed1cece6573bd926f4de6f2f622d6ae73a
Diaa Eldeen Esmail Ahmed <diaaeldean56@gmail.com>
2020-06-01 20:37:41 +0200

    Added time slot option to configuration

--- include/ERTOS_config.h
+++ include/ERTOS_config.h
@@ -12,5 +12,11 @@
 #define PRIORITY_LEVELS		9
 #define FPU_ENABLED	0
 
+/* This is used to control the time slot given for each function in round robin scheduling
+ * The number represents the number of ticks for each time slot
+ */
+#define TIME_SLOT	2
+
+
 
 #endif /* ERTOS_CONFIG_H_ */
